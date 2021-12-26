8a38c0ac359902d8d9a802291d0f3ec72eb550ff
ericviana <ericviana954@gmail.com>
2021-12-26 16:08:18 -0300

    Commit Inicial

---
+++ Battle.py
@@ -0,0 +1,11 @@
+class Battle:
+
+    def __init__(self, player_1: object = None, player_2: object = None):
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+    def player_1_atk_2(self):
+        self.player_2.p_hp = (self.player_2.p_hp - self.player_1.p_atk) + self.player_2.p_def
+
+    def player_2_atk_1(self):
+        self.player_1.p_hp = (self.player_1.p_hp - self.player_2.p_atk) + self.player_1.p_def
